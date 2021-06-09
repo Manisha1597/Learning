0aab9e0dac190fe72157390d3936d0faba5e8006
Manisha1597 <85611274+Manisha1597@users.noreply.github.com>
2021-06-09 19:26:27 +0530

    Create generate_inventory.sh

---
+++ generate_inventory.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+build_number=$1
+job_name=$2
+host_env=$3
+echo $1 $2
+
+echo "host env for adhoc is "
+echo $host_env
+
+echo "[windows]" > /var/jenkins_home/workspace/$job_name/inventory
+
+host=$(echo $host_env|sed 's/,/\n/g')
+
+echo "$host" >> /var/jenkins_home/workspace/$job_name/inventory
